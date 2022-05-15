d8b83126807da6edbdc3c02557d6acbb600584b3
Faris Dhoot <farisgjd@outlook.com>
2022-05-15 00:19:47 +0000

    "

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#j4a18i9rex2ai@@yf6&c*o(^p%3&3&b5rxb$9mryqb!)0-9oe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangofd-todo-app.herokuapp.com/']
+ALLOWED_HOSTS = ['djangofd-todo-app.herokuapp.com']
 
 
 # Application definition
